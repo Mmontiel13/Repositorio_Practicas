039ce128173a3a6ac4ed12c4fac6d332f9e2f034
josue <josuevj.1e.49@gmail.com>
2023-12-03 22:24:13 -0600

    ejercicio 1 ya inserta datos en la bd, falta logica para que coincida con las llaves foraneas

--- mini_Proyecto/ejercicio1/validandoXMLconXSD.php
+++ mini_Proyecto/ejercicio1/validandoXMLconXSD.php
@@ -1,11 +1,15 @@
 <?php
+//COMENTE LAS CONSULTAS DE INSERSION, PUES HASYA EL MOMENTO SE EJECUTAN CADA QUE SE ABRE EL ARCHIVO PHP
 libxml_use_internal_errors(true);
+
+//cargamos xml y el xsd
 $xml= new DOMDocument();
 $documento = file_get_contents('catalogovod.xml');
 $xml->loadXML($documento, LIBXML_NOBLANKS);
-// o usa $xml->load si prefieres usar la ruta del archivo
 $xsd = 'serviciovod_ns.xsd';
-if (!$xml->schemaValidate($xsd))// o usa $xml->schemaValidateSource si prefieres usar el xsd en format string
+
+
+if (!$xml->schemaValidate($xsd))//valida el xml con el xsd
 {
     $errors = libxml_get_errors();
     $noError = 1;
@@ -16,8 +20,16 @@ if (!$xml->schemaValidate($xsd))// o usa $xml->schemaValidateSource si prefieres
     }
     echo $lista;
 }
-else{
-    echo '<style>
+else{ //si todo esta bien...
+    $conexion = @mysqli_connect( //abrimos la conexion a la bd
+        'localhost',
+        'root',
+        'Normita1230',
+        'plataformavod'
+    );
+
+    //estilos para mostrar las peliculas y series en html
+    echo '<style> 
     
             body {
                 font-family: sans-serif;
@@ -62,6 +74,7 @@ else{
     echo '<body>';
     echo '<header><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Amazon_Prime_Video_logo.svg/2560px-Amazon_Prime_Video_logo.svg.png" style="width: 25em; height: auto;"/></header>';
     echo '<h1>Catalogo VOD</h1>';
+    //seleccionamos todos los nodos peliculas y series
     $peliculas = $xml->getElementsByTagName('peliculas');
     $series = $xml->getElementsByTagName('series');
     echo '<section>
@@ -73,6 +86,7 @@ else{
                     <th>G&eacute;nero</th>
                 </tr>
                 ';
+                //vamos descomponiendo la pelicula
                 foreach ($peliculas as $pelicula) {
                     $generos = $pelicula->getElementsByTagName('genero');
                     foreach ($generos as $genero) {
@@ -84,6 +98,12 @@ else{
                                     <td>' . $titulo->getAttribute('duracion') . '</td>
                                     <td>' . $genero->getAttribute('nombre') . '</td>
                                   </tr>';
+                                    $region = $pelicula->getAttribute('region');
+                                    $nombre_genero = $genero->getAttribute('nombre');
+                                    $nombre_titulo = $titulo->nodeValue;
+                                    $duracion = $titulo->getAttribute('duracion');
+                                    /*$sql = "INSERT INTO contenido VALUES (null, 'pelicula', '$region', '$nombre_genero', '$nombre_titulo', '$duracion', 0, 0)";  
+                                    mysqli_query($conexion, $sql); */
                         }
                     }
                 }
@@ -109,15 +129,41 @@ else{
                                     <td>' . $titulo->getAttribute('duracion') . '</td>
                                     <td>' . $genero->getAttribute('nombre') . '</td>
                                   </tr>';
+                                  $region = $serie->getAttribute('region');
+                                  $nombre_genero = $genero->getAttribute('nombre');
+                                  $nombre_titulo = $titulo->nodeValue;
+                                  $duracion = $titulo->getAttribute('duracion');
+                                  /*$sql = "INSERT INTO contenido VALUES (null, 'serie', '$region', '$nombre_genero', '$nombre_titulo', '$duracion', 0, 0)";  
+                                  mysqli_query($conexion, $sql); */
                         }
                     }
                 }
         echo '</table>';
     echo '</section>';
     echo '</body>';
+    
+    //sacamos el correo y lo insertamos en la tabla cuenta
+    $cuentas = $xml->getElementsByTagName('cuenta');
+    foreach($cuentas as $cuenta){
+        $correo = $cuenta->getAttribute('correo');
+        /*$sql = "INSERT INTO cuenta VALUES (null, '$correo', 0)";  
+        mysqli_query($conexion, $sql); */
+    }
 
-    // Guardar el documento modificado en un nuevo archivo o sobrescribir el existente
-    //$xml->save('nuevo_ejemplo.xml');
+    //sacamos los perfiles y lo insertamos en la tabla perfiles
+    //el ultimo 0 es la llave foranea que corresponde a la cuenta, seria bueno que hubiera una consulta que rectificara la existencia de esa cuenta.
+    $perfiles = $xml->getElementsByTagName('perfil');
+    foreach($perfiles as $perfil){
+        $usuario = $perfil->getAttribute('usuario');
+        $idioma = $perfil->getAttribute('idioma');
+        /*$sql = "INSERT INTO perfiles VALUES (null, '$usuario', '$idioma', 0, 0)";  
+        mysqli_query($conexion, $sql);*/
+    }
+    $conexion->close();
+
+   
+               
+    
 }
 
 ?>
